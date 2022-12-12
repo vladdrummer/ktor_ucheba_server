5f0542f626ba280d29e5e2cf1cd22585c4e935ee
vladdrummer <vladdrummervlad@yandex.ru>
2022-12-12 13:16:33 +0300

    App changed

--- src/main/kotlin/com/vladdrummer/Application.kt
+++ src/main/kotlin/com/vladdrummer/Application.kt
@@ -7,12 +7,12 @@ import com.vladdrummer.plugins.*
 import io.ktor.server.auth.*
 
 fun main() {
-    embeddedServer(Netty, port = 4011) {
+    embeddedServer(Netty, port = 80) {
         install(Authentication) {
             basic("auth-basic") {
                 realm = "Access to the '/' path"
                 validate { credentials ->
-                    if (credentials.name == "login" && credentials.password == "pass") {
+                    if (credentials.name == "login@login.com" && credentials.password == "pass") {
                         UserIdPrincipal(credentials.name)
                     } else {
                         null
